400fa564e4cd4bca6058d477b998bee4b59698b4
Paweł Zaremba <pawzar@gmail.com>
2023-09-11 23:44:03 +0200

    fix typo

--- feeds/k3s-install/install.sh
+++ feeds/k3s-install/install.sh
@@ -191,8 +191,8 @@ function collect_vars {
     # Generate the values.yaml file
     cat <<EOF | sudo tee /opt/chronicle/"${FEED_NAME}"/generated-values.yaml
 ghost:
-   service:
-     type: LoadBalancer
+  service:
+    type: LoadBalancer
   ethConfig:
     ethFrom:
       existingSecret: '$FEED_NAME-eth-keys'
@@ -213,8 +213,8 @@ ghost:
   chainId: 1
 
 musig:
-   service:
-     type: LoadBalancer
+  service:
+    type: LoadBalancer
   ethConfig:
     ethFrom:
       existingSecret: '$FEED_NAME-eth-keys'
