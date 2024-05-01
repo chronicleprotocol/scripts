53d8916c23c14d45cb1e5ba06b14dce30ba4dd01
Wesley Charles Blake <wesley@chroniclelabs.org>
2024-05-01 19:51:29 +0200

    fix validate_dot_env function and remove break in validate_command

--- feeds/k3s-install/upgrade.sh
+++ feeds/k3s-install/upgrade.sh
@@ -19,24 +19,45 @@ display_usage() {
     echo "======"
     echo "./upgrade.sh"
     echo "# export FEED_NAME=<MY_FEED_NAME>"
-    echo "can be set in .env file or exporter in the environment"
-    echo "required: FEED_NAME"
+    echo "can be set in .env file or exported in the environment"
+    echo "required variables: FEED_NAME"
 }
 
-# Source the .env file if it exists
+# Source the .env file if it exists and prompt for FEED_NAME if not set
 validate_dot_env() {
-    if [ -f ".env" ]; then
-        source .env
-        [[ -n "${FEED_NAME:-}" ]] && echo "FEED_NAME: $FEED_NAME" | tee -a "$LOG_FILE"
+    # Check if FEED_NAME is already set in the environment
+    if [[ -n "${FEED_NAME:-}" ]]; then
+        echo "FEED_NAME: $FEED_NAME" | tee -a "$LOG_FILE"
     else
-        echo -e "\e[31m[ERROR]: Unable to locate .env file!\e[0m" | tee -a "$LOG_FILE"
-        display_usage
+        # Check if .env file exists
+        if [ -f ".env" ]; then
+            source .env
+            # Check if FEED_NAME is set after sourcing .env
+            if [[ -n "${FEED_NAME:-}" ]]; then
+                echo "FEED_NAME: $FEED_NAME" | tee -a "$LOG_FILE"
+            else
+                # Prompt the user for FEED_NAME if still not set
+                echo -e "\e[33m[WARNING]: FEED_NAME is not set in .env file.\e[0m" | tee -a "$LOG_FILE"
+                read -rp "Enter FEED_NAME: " FEED_NAME
+                if [[ -n "${FEED_NAME:-}" ]]; then
+                    echo "FEED_NAME: $FEED_NAME" | tee -a "$LOG_FILE"
+                else
+                    echo -e "\e[31m[ERROR]: FEED_NAME cannot be empty! Exiting...\e[0m" | tee -a "$LOG_FILE"
+                    exit 1
+                fi
+            fi
+        else
+            echo -e "\e[33m[WARNING]: .env file not found. Prompting for FEED_NAME.\e[0m" | tee -a "$LOG_FILE"
+            read -rp "Enter FEED_NAME: " FEED_NAME
+            if [[ -n "${FEED_NAME:-}" ]]; then
+                echo "FEED_NAME: $FEED_NAME" | tee -a "$LOG_FILE"
+            else
+                echo -e "\e[31m[ERROR]: FEED_NAME cannot be empty! Exiting...\e[0m" | tee -a "$LOG_FILE"
+                exit 1
+            fi
+        fi
     fi
-    if [[ -z "${FEED_NAME:-}" ]]; then
-        echo -e "\e[31m[ERROR]: FEED_NAME is not set! Exiting...\e[0m" | tee -a "$LOG_FILE"
-        display_usage
-        exit 1
-    fi
+
     echo "OS Version: $(lsb_release -rs)" | tee -a "$LOG_FILE"
     echo "User: $USER" | tee -a "$LOG_FILE"
     echo "Date: $(date)" | tee -a "$LOG_FILE"
@@ -48,7 +69,6 @@ validate_user() {
         echo -e "\e[31m[ERROR]: This script should not be run as root!\e[0m"
         echo -e "\e[31m[ERROR]: Please run the script as a non-root user with sudo privileges.\e[0m"
         echo -e "\e[31m[ERROR]: Switch to the user created by the installer script and re-run the script.\e[0m" | tee -a "$LOG_FILE"
-        display_usage
         exit 1
     fi
 }
@@ -57,7 +77,6 @@ validate_user() {
 validate_command() {
     command -v yq > /dev/null 2>&1 || {
         echo -e "\e[31m[ERROR]: yq is not installed!\e[0m"  | tee -a "$LOG_FILE"
-        exit 1
         echo -e "\e[32m[INFO]:..........Installing yq.........\e[0m"
         sudo apt-get update -y
         sudo apt-get install -y yq
@@ -97,14 +116,14 @@ sanitize_values() {
 create_helm_upgrade() {
     # helm upgrade $FEED_NAME with --debug and --dry-run
     echo -e "\e[32m[INFO]:..........DRY RUN UPGRADE feed: $FEED_NAME in namespace: $FEED_NAME.........\e[0m"
-    helm upgrade "$FEED_NAME" -f "$HOME/$FEED_NAME/generated-values.yaml" chronicle/validator --namespace "$FEED_NAME" --debug --dry-run
+    helm upgrade "$FEED_NAME" -f "$HOME/$FEED_NAME/generated-values.yaml" chronicle/validator --namespace "$FEED_NAME" --debug --dry-run | tee -a "$LOG_FILE"
 
     # prompt user to confirm upgrade, accept a Y/N
     echo -e "\e[33m[NOTICE]: DRY RUN UPGRADE complete! Do you want to continue with the upgrade? (y/n): "
-    read -r response
+    read -r response | tee -a "$LOG_FILE"
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
         echo -e "\e[33m[NOTICE]: Upgrading feed: $FEED_NAME in namespace: $FEED_NAME.\e[0m"
-        helm upgrade "$FEED_NAME" -f "$HOME/$FEED_NAME/generated-values.yaml" chronicle/validator --namespace "$FEED_NAME"
+        helm upgrade "$FEED_NAME" -f "$HOME/$FEED_NAME/generated-values.yaml" chronicle/validator --namespace "$FEED_NAME" | tee -a "$LOG_FILE"
     else
         echo -e "\e[33m[NOTICE]: Terminating the script as per user request.\e[0m"
         # print the helm command they will need to run
